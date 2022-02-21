13fd4203aec966adbdb846f8f99db5e248ed6f67
Ionut Banu <ionutbanu094@gmail.com>
2022-02-21 19:36:51 +0200

    error handling

---
+++ src/main/java/com/example/sr_c2_e2/exception/ProductRetrieveException.java
@@ -0,0 +1,4 @@
+package com.example.sr_c2_e2.exception;
+
+public class ProductRetrieveException extends RuntimeException {
+}
--- src/main/java/com/example/sr_c2_e2/proxy/ProductProxy.java
+++ src/main/java/com/example/sr_c2_e2/proxy/ProductProxy.java
@@ -15,7 +15,26 @@ public class ProductProxy {
     private final WebClient webClient;
 
     public Flux<Product> getAll() {
+        var p = new Product("Default");
         return webClient.get().uri("/product")
-                        .exchangeToFlux(res -> res.bodyToFlux(Product.class));
+                        .exchangeToFlux(res -> res.bodyToFlux(Product.class))
+                        //                          .onErrorResume(e -> Flux.just(p));
+                        //                          .onErrorResume(WebClientRequestException.class, e -> Flux.just(p));
+                        //                          .onErrorResume(e -> e instanceof WebClientRequestException, e -> Flux.just(p));
+                        //                          .onErrorReturn(p);
+                        //                          .onErrorReturn(WebClientRequestException.class, p);
+                        //                          .onErrorReturn(e -> e.getMessage() == null, p);
+                        //                          .onErrorMap(e -> new ProductRetrieveException());
+                        //                          .doOnNext(n -> {
+                        //                              if (n.getName() == null) {
+                        //                                  throw new RuntimeException();
+                        //                              }
+                        //                          })
+                        //                          .onErrorContinue((e, o) -> System.out.println(e.getMessage()));
+                        //                          .onErrorContinue(RuntimeException.class, (e, o) -> System.out.println(e.getMessage()));
+                        //                          .onErrorContinue(e->e.getMessage() == null, (e, o) -> System.out.println(e.getMessage()));
+                        //                          .retry();
+                        .retry(3);
+
     }
 }
